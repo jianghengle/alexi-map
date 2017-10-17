ad6f60e8e26eff1abeec9dc8d73c909ced265c14
Hengle Jiang <jianghengle@gmail.com>
2017-10-17 12:24:44 -0500

    add another three logos

--- src/components/AlexiFooter.vue
+++ src/components/AlexiFooter.vue
@@ -3,11 +3,13 @@
     <footer class="footer">
   <div class="container">
     <div class="content has-text-centered">
-      <img src="static/logos/DWFI.png" width="250" class="logo">
+      <img src="static/logos/DWFI.png" height="250" class="logo">
       <img src="static/logos/USDA ARS.png" width="250" class="logo">
-      <img src="static/logos/NASA.png" width="80" class="logo">
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <img src="static/logos/ESSIC.jpg" width="150" class="logo">
+      <img src="static/logos/NASA.png" width="100" class="logo">
+      <img src="static/logos/ESSIC.jpg" width="150" class="logo essic">
+      <img src="static/logos/NDMC.png" width="400" class="logo">
+      <img src="static/logos/USAID.png" width="300" class="logo usaid">
+      <img src="static/logos/ICBA.png" width="150" class="logo icba">
     </div>
   </div>
 </footer>
@@ -28,5 +30,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.essic {
+  margin-left: 30px;
+  margin-right: 20px;
+}
+
+.usaid {
+  position: relative;
+  top: 30px;
+}
+
+.icba {
+  position: relative;
+  top: -10px;
+}
 
 </style>
