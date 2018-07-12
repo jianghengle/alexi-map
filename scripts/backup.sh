3149c58ed229fe8517e6f0cd4db7b3c720006691
Hengle Jiang <jianghengle@gmail.com>
2018-07-12 14:30:25 -0500

    fix bug

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -4,6 +4,4 @@
 /usr/bin/pg_dump $PG_URL > $ALEXI_ROOT/../db-backup/db.dump
 
 # transfer to attic
-rsync -avz $ALEXI_ROOT/../db-backup hjiang5@attic.unl.edu:/attic/waterforfood/hjiang5/glodet_backup/db-backup
-rsync -avz $ALEXI_ROOT hjiang5@attic.unl.edu:/attic/waterforfood/hjiang5/glodet_backup/alexi-data
-rsync -avz $ALEXI_ROOT/../gdl-data hjiang5@attic.unl.edu:/attic/waterforfood/hjiang5/glodet_backup/gdl-data
+rsync -avz $ALEXI_ROOT/../db-backup $ALEXI_ROOT/../gdl-data $ALEXI_ROOT hjiang5@attic.unl.edu:/attic/waterforfood/hjiang5/glodet_backup --delete
