1d4d768ec6724dcac1bb11d86313fb8221502b33
Hengle Jiang <jianghengle@gmail.com>
2018-10-23 15:49:47 -0500

    upgrade crystal 0.26-1

--- client/src/components/AlexiHeader.vue
+++ client/src/components/AlexiHeader.vue
@@ -73,7 +73,7 @@
     </nav>
     <div class="color-seperator"></div>
     <div class="notification is-danger">
-      NOTICE: The GloDET data viewer is currently still under development.  Any questions or comments are welcome at <a href="mailto:glodet@nebraska.edu">glodet@nebraska.edu</a>.  Thank you for visiting.
+      NOTICE: The GloDET data viewer is currently still under development. New user accounts will be approved once development is complete.  Any questions or comments are welcome at <a href="mailto:glodet@nebraska.edu">glodet@nebraska.edu</a>.  Thank you for visiting.
     </div>
   </div>
 </template>
